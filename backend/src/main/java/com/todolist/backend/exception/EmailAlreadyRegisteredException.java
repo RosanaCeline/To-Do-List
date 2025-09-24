5d4897c64c8cd5a0effedbda66b544a42e302b96
Rosana Celine <celine.rosana@gmail.com>
2025-09-24 12:32:53 -0300

    refactor: update email already registered exception

--- backend/src/main/java/com/todolist/backend/exception/EmailAlreadyRegisteredException.java
+++ backend/src/main/java/com/todolist/backend/exception/EmailAlreadyRegisteredException.java
@@ -1,7 +1,15 @@
 package com.todolist.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmailAlreadyRegisteredException extends RuntimeException {
-  public EmailAlreadyRegisteredException(String message) {
-    super(message);
-  }
+    public EmailAlreadyRegisteredException() {
+        super("E-mail já está cadastrado.");
+    }
+
+    public EmailAlreadyRegisteredException(String message) {
+        super(message);
+    }
 }
